77b1c821ad50e96fd8cdfca41bf390779ca07982
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-07 21:13:58 +0000

    written first working test - checking only 1 page of results are being returned from Flickr

--- spec/flickr-spec.js
+++ spec/flickr-spec.js
@@ -1,37 +1,19 @@
 /// spec/flickr-spec.js
 'use strict'
-const flickrSearch = require('../modules/flickr')
-const jasmine = require('jasmine')
-const getFlickrData = require('../server')
-const request = require('request')
-const base_url = 'http://localhost:8081/'
-/*describe('Searching Location', function () {
-    it('should put the location into the Flickr API URL', function () {
-        const tag = 'London'
-        const location = getFlickrData(tag);
-        expect(location).toBe('London');
-    });
-});
-describe('Searching 2 photos per page', function () {
-    it('should display 2 photos', function () {
-        const location = getFlickrData(tag);
-        expect(JSON.photos.perpage).toBe(2);
-    });
-});*/
-describe("Testing Server is running", function () {
-    describe("GET /", function () {
-        it("returns status code 200", function () {
-            request.get(base_url, function (error, response, body) {
-                expect(response.statusCode).toBe(200)
-                expect(true).toEqual(true)
-                done()
+const flickr = require('../modules/flickr')
+describe('Number of pages', function () {
+            it('should limit the number to 1', function (done) {
+                const tag = 'London'
+                flickr.flickrSearch(tag, function (err, searchResults) {
+                    expect(searchResults.photos.perpage).toBe(2)
+                    done()
+                });
             });
-        });
-    });
-});
-///tests
-///when searching for london, the url contains tags="london"
-///when searching, 2 photos will be returned = per_page=2
-///when searching, and no photos show up - undefinded
-///
+})
 
+
+            ///tests
+            ///when searching for london, the url contains tags="london"
+            ///when searching, 2 photos will be returned = per_page=2
+            ///when searching, and no photos show up - undefinded
+            ///
