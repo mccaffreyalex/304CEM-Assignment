ac3f8095e0114ac75bd653dfed0961e177b4cc98
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-07 16:11:22 +0000

    creating first test for flickr.api

--- spec/flickr-spec.js
+++ spec/flickr-spec.js
@@ -1,8 +1,19 @@
+/// spec/flickr-spec.js
 'use strict'
-const flickr = require('../modules/flickr')
-const address = 'London'
-describe('Searching Location', () => {
-    it('should put the locaion into the Flickr API URL', () => {});
-    flickr.add('London')
-    expect()
+const flickrSearch = require('../modules/flickr')
+const jasmie = require('jasmine')
+const tag = 'London'
+const getFlickrData = require('../server')
+describe('Searching Location', function () {
+    it('should put the location into the Flickr API URL', function () {
+        const location = getFlickrData(tag);
+        expect(location).toBe('London');
+    });
+});
+
+describe('Searching 2 photos per page', function () {
+    it('should display 2 photos', function () {
+        const location = getFlickrData(tag);
+        expect(JSON.photos.perpage).toBe(2);
+    });
 });
