c2eafda29f6a55ef693c77b76f83bf66e32046d0
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-07 15:07:59 +0000

    creating first test for Flickr api

---
+++ spec/flickr-spec.js
@@ -0,0 +1,8 @@
+'use strict'
+const flickr = require('../modules/flickr')
+const address = 'London'
+describe('Searching Location', () => {
+    it('should put the locaion into the Flickr API URL', () => {});
+    flickr.add('London')
+    expect()
+});
