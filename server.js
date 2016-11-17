f5c4610ad0ed03cfdb36a0d44cc250b2370fd7df
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-11-17 17:41:48 +0000

    added /weather and /flickr into url

--- server.js
+++ server.js
@@ -3,25 +3,16 @@
 const restify = require('restify')
     , fs = require('fs')
     , ejs = require('ejs')
-    , flickr = require('./modules/flickr')
+    , flickr = require('./modules/flickr-request')
+    , weatherRequest = require('./modules/weather-request')
     , port = process.env.PORT || 8081
     , server = restify.createServer({
-        name: 'restify server'
+        name: '304CEM Restify Server'
     })
-server.use(function (req, res, next) {
-    console.log(req.method + ' ' + req.url)
-    return next()
-})
+server.use(restify.queryParser())
+server.use(restify.acceptParser(server.acceptable))
+server.use(restify.bodyParser())
 
-function test(req, res, next) {
-    var testpage = fs.readFileSync('views/index.ejs', 'UTF-8')
-    res.end(ejs.render(testpage))
-    return next()
-}
-/*function getWeather(req, res, next) {
-	var image_id = req.params.image_id
-	var testpage = fs.readFileSync('views/details.ejs', 'UTF-8')
-}*/
 function getFlickrData(req, res, next) {
     var address = req.params.address
     var testPage = fs.readFileSync('client/search.html', 'UTF-8')
@@ -37,17 +28,40 @@ function getFlickrData(req, res, next) {
         }
     })
 }
-server.use(restify.bodyParser())
-server.get('/', getIndex)
-server.get('test', test)
-    ///server.get('details', getWeather)
-server.post('results', getFlickrData)
-    ///server.post('results', getFlickrInfo);
+
 function getIndex(req, res, next) {
     var indexPage = fs.readFileSync('client/index.html', 'UTF-8')
     res.end(ejs.render(indexPage))
     return next()
 }
+server.use(function (req, res, next) {
+    console.log(req.method + ' ' + req.url)
+    return next()
+})
+server.get('/weather', function (req, res, next) {
+    console.log(req.params.q)
+    console.log(req.params.dt)
+    weatherRequest.weatherSearch(req.params.q, req.params.dt, function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send(result)
+        }
+    })
+})
+server.get('/flickr', function (req, res, next) {
+    console.log(req.params.t)
+    flickr.flickrSearch(req.params.t, function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send(result)
+        }
+    })
+})
+server.post('results', getFlickrData)
 server.listen(port, function () {
     console.log('API running at port: ' + port + '. Go to localhost:8081')
 })
