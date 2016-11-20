0a47c7d15561f8540ce66ddedb211c789f66d5be
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-20 17:25:38 +0000

    updated

--- modules/weather-request.js
+++ modules/weather-request.js
@@ -11,7 +11,12 @@ exports.weatherSearch = function (location, date, callback) {
         }
     }
     request(options, function (err, res, body) {
+        //Check for error
         if (err) throw new Error(err);
+        const json = JSON.parse(body)
+            ///console.log(JSON.stringify(json, null, 2))
+        console.log(json.location)
+            ///console.log(json.forecast.forecastday[0].date)
         return callback(null, JSON.parse(body))
     })
 }
