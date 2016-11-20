898593bf60d3bd96cd26fc15cb766e27fdc0ae77
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-20 17:25:04 +0000

    updated to include /weather-request

--- server2.js
+++ server2.js
@@ -1,12 +1,27 @@
 'use strict'
 const restify = require('restify')
-    , weatherRequest = require('./modules/weather-request')
-    , server = restify.createServer()
+const weatherRequest = require('./modules/weather-request')
+const flickr = require('./modules/flickr-request')
+const flickrInfo = require('./modules/flickr-info-request')
+const server = restify.createServer({
+    name: '304CEM Restify Server'
+})
+const defaultPort = 8081
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.acceptParser(server.acceptable))
-const defaultPort = 8081
-server.get('/', function (req, res, next) {
+server.get('/flickr', function (req, res, next) {
+    console.log(req.params.t)
+    flickr.flickrSearch(req.params.t, function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send(result)
+        }
+    })
+})
+server.get('/weather', function (req, res, next) {
     console.log(req.params.q)
     console.log(req.params.dt)
     weatherRequest.weatherSearch(req.params.q, req.params.dt, function (err, result) {
@@ -18,13 +33,23 @@ server.get('/', function (req, res, next) {
         }
     })
 })
-
+server.get('/flickr-info', function (req, res, next) {
+    console.log(req.params.i)
+    flickrInfo.flickrInfo(req.params.i, function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send(result)
+        }
+    })
+})
 const port = process.env.PORT || defaultPort
-server.listen(port, function(err){
+server.listen(port, function (err) {
     if (err) {
         console.log(err)
     }
     else {
-        console.log('App running at: ' + port)
+        console.log('Server running at: ' + port)
     }
 })
