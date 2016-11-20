023a4b4ab5b42ebc097f0c166291ed6c870f098c
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-20 17:26:22 +0000

    updated to include the flickr-request

--- spec/flickr-spec.js
+++ spec/flickr-spec.js
@@ -1,40 +1,39 @@
 /*global require describe it xit expect */
 /*no eslint errors*/
 'use strict'
-const flickr = require('../modules/flickr')
+const flickr = require('../modules/flickr-request')
 describe('The Flickr API', function () {
-	it('should limit the number to 2', function (done) {
-		const tag = 'London'
-		flickr.flickrSearch(tag, function (err, searchResults) {
-			expect(searchResults.photos.perpage).toBe(2)
-			expect(searchResults.stat).toBe('ok')
-			done()
-		})
-	})
-	it('should have status of 200', function (done) {
-		const tag = 'London'
-		flickr.flickrSearch(tag, function (err, searchResults) {
-			expect(searchResults.stat).toBe('ok')
-			done()
-		})
-	})
-	xit('should contain the tag in the url', function (done) {
-		const tag = 'London'
-		flickr.flickrSearch(tag, function (err, searchResults) {
-			expect(searchResults.fullURL).toContain('tags=London')
-			done()
-		})
-	})
-	it('should return 0 pages when no photos for the tag can be found', function (done) {
-		const tag = 'randomtaghere' ///random tag
-		flickr.flickrSearch(tag, function (err, searchResults) {
-			expect(searchResults.photos.pages).toBe(0)
-			done()
-		})
-	})
-})
+        it('should limit the number to 2', function (done) {
+            flickr.flickrSearch('London', function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(searchResults.photos.perpage).toBe(2)
+                expect(searchResults.stat).toBe('ok')
+                done()
+            })
+        })
+        it('should have status of ok', function (done) {
+            flickr.flickrSearch('London', function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(searchResults.stat).toBe('ok')
+                done()
+            })
+        })
+        it('should return 0 pages when no photos for the tag can be found', function (done) {
+            flickr.flickrSearch('randomtaghere', function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(searchResults.photos.pages).toBe(0)
+                done()
+            })
+        })
+        xit('should contain the tag in the URL', function (done) {
+            flickr.flickrSearch('London', function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(flickr.fullURL).toContain('tags=London')
+                done()
+            })
+        })
+    })
     ///tests
     ///when searching for london, the url contains tags="london"
-    ///when searching, 2 photos will be returned = per_page=2
     ///when searching, and no photos show up - undefinded
     ///
