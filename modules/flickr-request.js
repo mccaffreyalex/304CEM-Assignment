155924bad13c93f1c7452331ccd896da52065494
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-20 17:24:29 +0000

    changed per_page to 2

--- modules/flickr-request.js
+++ modules/flickr-request.js
@@ -9,13 +9,17 @@ exports.flickrSearch = function (tag, callback) {
             , api_key: '47b47ea9fe9d92ff9aac9cc70acb388a'
             , tags: tag
             , min_upload_date: '1477958400'
-            , per_page: '1'
+            , per_page: '2'
             , format: 'json'
             , nojsoncallback: '1'
         }
     }
+    console.log('You searched for ' + tag)
     request(options, function (err, res, body) {
+        //Check for error
         if (err) throw new Error(err);
         return callback(null, JSON.parse(body))
     })
 }
+
+// id = 24824103133
