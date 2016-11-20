d1a0c8b7d075d5570b39d936fc5e6edcd9bbc840
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-20 17:25:23 +0000

    added done() to make the test finish

--- spec/weather-spec.js
+++ spec/weather-spec.js
@@ -1,38 +1,30 @@
 /*global require describe it xit expect */
 'use strict'
-const weather = require('../modules/weather')
+const weather = require('../modules/weather-request')
 describe('The Apixu Weather API', function () {
-    it('should check that the request was successful', function (done) {
-        const location = 'London'
-        const date = '2016-11-07'
-        weather.weatherSearch(location, date, function (err, searchResults) {
-            expect(searchResults.location.name).toBe(location)
-            expect(searchResults.forecast.forecastday[0].date).toBe(date)
+        it('should check that the request was successful', function (done) {
+            weather.weatherSearch('London', '2016-11-07', function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(searchResults.location.name).toBe('London')
+                expect(searchResults.forecast.forecastday[0].date).toBe('2016-11-07')
+                done()
+            })
+        })
+        it('should return an error if location missing', function (done) {
+            weather.weatherSearch('', '2016-11-08', function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(searchResults.error.code).toBe(1003)
+                expect(searchResults.error.message).toBe('Parameter q is missing.')
+                done()
+            })
+        })
+        it('should return an error if date missing', function (done) {
+            weather.weatherSearch('London', '', function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(searchResults.error.code).toBe(1007)
+                expect(searchResults.error.message).toContain('dt parameter should be in yyyy-MM-dd')
+                done()
+            })
         })
     })
-    it('should return an error if location missing', function (done) {
-        const location = ''
-        const date = '2016-11-07'
-        weather.weatherSearch(location, date, function (err, searchResults) {
-            expect(searchResults.error.code).toBe(1003)
-            expect(searchResults.error.message).toBe(Parameter q is missing)
-        })
-    })
-    it('should return an error if date missing', function (done) {
-        const location = 'London'
-        const date = ''
-        weather.weatherSearch(location, date, function (err, searchResults) {
-            expect(searchResults.error.code).toBe(1007)
-            expect(searchResults.error.message).toContain('dt parameter should be in yyyy-MM-dd')
-        })
-    })
-    it('should return status 200 OK to check the API key is valid', function(done){
-        const location = 'London'
-        const date = '2016-11-07'
-        weather.weatherSearch(location, date, function(err, searchResults){
-            expect(searchResults.status).toBe(200)
-        })
-    })
-})
-
-//to run type npm test
+    //to run type npm test
