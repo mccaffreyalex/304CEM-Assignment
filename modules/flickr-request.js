5e1e20a3522cc2fbad0b5722cc8b90acadcd1b47
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-11-24 17:07:42 +0000

    changed exports.flickrSearch layout

--- modules/flickr-request.js
+++ modules/flickr-request.js
@@ -1,25 +1,18 @@
 'use strict'
 const request = require('request')
-exports.flickrSearch = function (tag, callback) {
-    const options = {
-        method: 'GET'
-        , url: 'https://api.flickr.com/services/rest/'
-        , qs: {
-            method: 'flickr.photos.search'
-            , api_key: '47b47ea9fe9d92ff9aac9cc70acb388a'
-            , tags: tag
-            , min_upload_date: '1477958400'
-            , per_page: '2'
-            , format: 'json'
-            , nojsoncallback: '1'
+exports.flickrSearch = function flickrSearch(req, res, next) {
+    const tag = req.query.q
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=47b47ea9fe9d92ff9aac9cc70acb388a&tags=${tag}&min_upload_date=1477958400&per_page=1&format=json&nojsoncallback=1`
+    request.get(url, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+            const books = []
+            for (let i = 0; i < 1; i++) {
+                let book = {
+                    id: JSON.parse(body)
+                }
+                books.push(book)
+            }
+            res.send(books)
         }
-    }
-    console.log('You searched for ' + tag)
-    request(options, function (err, res, body) {
-        //Check for error
-        if (err) throw new Error(err);
-        return callback(null, JSON.parse(body))
     })
 }
-
-// id = 24824103133
