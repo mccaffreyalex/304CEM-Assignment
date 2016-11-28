96f3a7e6bb215266e411c9bc49401952d6c97d44
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-11-28 21:00:10 +0000

    trying to create routes

--- modules/data-api.js
+++ modules/data-api.js
@@ -1,12 +1,27 @@
 'use strict'
 const flickr = require('./flickr-request')
-const flickrInfo = require('./flickr-info')
+const flickrInfo = require('./flickr-info-request')
 const weather = require('./weather-request')
+exports.search = (request, callback) => {
+    extractParam(request, 't').then(query => {
+        return flickr.searchByTag(query)
+    }).then(data => {
+        callback(null, data)
+    }).catch(err => {
+        callback(err)
+    })
+}
+exports.searchID = (request, callback) => {
+    extractParam(request, 'i').then(query => {
+        return flickrInfo.searchByID(query)
+    }).then(data => {
+        callback(null, data)
+    }).catch(err => {
+        callback(err)
+    })
+}
+const extractParam = (request, param) => new Promise((resolve, reject) => {
+    if (request.params === undefined || request.params[param] === undefined) reject(new Error(`${param} parameter missing`))
+    resolve(request.params[param])
+})
 
-///takes user location and puts into flickr api
-///waits for response, uses photo_id to search flickrInfo api
-///waits for response, takes date from flickrInfo and puts into weather api alongside location from step 1
-
-
-
-//returns all results in one page
