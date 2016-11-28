b45d00c2b1de3674ee5d098aba04819726152dc1
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-11-28 15:25:11 +0000

    written first test for user module

--- spec/users-spec.js
+++ spec/users-spec.js
@@ -1,8 +1,17 @@
 'use strict'
-
-///check user is added
-///check user is not added
-///check user is confirmed
-///check user is not confirmed
-///check user deleted
-///check user not deleted
+const user = require('../modules/users')
+describe('The add user function', function () {
+        it('confirm the user has been added, but not confirmed', function (done) {
+            user.validateUser('alex', , function (err, searchResults) {
+                expect(err).toBe(null)
+                expect(searchResults.photo.id).toBe('24824103133')
+                done()
+            })
+        })
+    })
+    ///check user is added
+    ///check user is not added
+    ///check user is confirmed
+    ///check user is not confirmed
+    ///check user deleted
+    ///check user not deleted
