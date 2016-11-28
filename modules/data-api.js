245e70bada4c8f1114ba688b384b766a77c45da3
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-11-28 15:25:30 +0000

    structure of the file

--- modules/data-api.js
+++ modules/data-api.js
@@ -3,4 +3,10 @@ const flickr = require('./flickr-request')
 const flickrInfo = require('./flickr-info')
 const weather = require('./weather-request')
 
+///takes user location and puts into flickr api
+///waits for response, uses photo_id to search flickrInfo api
+///waits for response, takes date from flickrInfo and puts into weather api alongside location from step 1
 
+
+
+//returns all results in one page
