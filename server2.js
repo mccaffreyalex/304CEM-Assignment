67f2c4409c5b8a9bab96e226d6b5be01bf6d0632
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-11-28 15:24:05 +0000

    updated

--- server2.js
+++ server2.js
@@ -1,26 +1,20 @@
 'use strict'
+/////////
 const restify = require('restify')
-const weatherRequest = require('./modules/weather-request')
+const server = restify.createServer()
 const flickr = require('./modules/flickr-request')
 const flickrInfo = require('./modules/flickr-info-request')
-const server = restify.createServer({
-    name: '304CEM Restify Server'
-})
-const defaultPort = 8081
+const weatherRequest = require('./modules/weather-request')
+const users = require('./modules/users')
+const authorization = require('./modules/auth')
+/////////
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.acceptParser(server.acceptable))
-server.get('/flickr', function (req, res, next) {
-    console.log(req.params.t)
-    flickr.flickrSearch(req.params.t, function (err, result) {
-        if (err) {
-            console.log(err)
-        }
-        else {
-            res.send(result)
-        }
-    })
-})
+server.use(restify.authorizationParser())
+/////////
+server.get('/flickr', flickr.flickrSearch)
+
 server.get('/flickr-info', function (req, res, next) {
     console.log(req.params.i)
     flickrInfo.flickrInfo(req.params.i, function (err, result) {
@@ -32,6 +26,9 @@ server.get('/flickr-info', function (req, res, next) {
         }
     })
 })
+
+server.get('/flickr-info', flickrInfo.flickrInfo)
+
 server.get('/weather', function (req, res, next) {
     console.log(req.params.q, req.params.dt)
     weatherRequest.weatherSearch(req.params.q, req.params.dt, function (err, result) {
@@ -43,12 +40,10 @@ server.get('/weather', function (req, res, next) {
         }
     })
 })
-const port = process.env.PORT || defaultPort
-server.listen(port, function (err) {
-    if (err) {
-        console.log(err)
-    }
-    else {
-        console.log('Server running at: ' + port)
-    }
-})
+///////
+server.post('/users', users.validateUser, users.add)  // add a new user to the DB (pending confirmation)
+server.post('/users/confirm/:username', users.validateCode, users.confirm)  // confirm a pending user
+server.del('/users/:username', authorization.authorize, users.delete)  // delete a user
+///////
+const port = process.env.PORT || 8081
+server.listen(port, err => console.log(err || `Server running at: ${port}`))
