1c5fb358a6fd785bb5ab1a27a1e53d490f71fd5c
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-11-29 10:27:13 +0000

    created searchWeatherByLocationDate promise

--- modules/weather-request.js
+++ modules/weather-request.js
@@ -14,29 +14,9 @@ exports.weatherSearch = function (location, date, callback) {
         //Check for error
         if (err) throw new Error(err);
         const json = JSON.parse(body)
+            ///console.log(JSON.stringify(json, null, 2))
         console.log(json.location)
+            ///console.log(json.forecast.forecastday[0].date)
         return callback(null, JSON.parse(body))
     })
 }
-exports.searchWeatherByLocationDate = query => new Promise((resolve, reject) => {
-    const url = `http://api.apixu.com/v1/history.json?key=ceac95eb930640de828164828162610&q=${query}&dt=${query}`
-    console.log(url)
-    request.get(url, (err, res, body) => {
-        if (err) {
-            reject(Error('API call failed'))
-        }
-        console.log(url)
-        const json = JSON.parse(body)
-        resolve(json)
-        const data = {
-            location: `${json.location.name}`
-            , country: `${json.location.country}`
-            , date: `${json.forecast.forecastday[0].date}`
-            , weather: `${json.forecast.forecastday[0].day.condition.text}`
-            , weatherURL: `${json.forecast.forecastday[0].day.condition.icon}`
-            , sunRise: `${json.forecast.forecastday[0].astro.sunrise}`
-            , sunSet: `${json.forecast.forecastday[0].astro.sunset}`
-        , }
-        resolve(data)
-    })
-})
