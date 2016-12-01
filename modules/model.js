9d50f51c30bd33fe75aadccb2b70da4abdba5a64
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-12-01 19:53:26 +0000

    addUser function

--- modules/model.js
+++ modules/model.js
@@ -1,6 +1,6 @@
 'use strict'
 const auth = require('./auth')
-const db = require('./db')
+const db = require('./persistence')
 const api = require('./data-api')
 exports.searchByTag = (request, callback) => {
     api.extractParam(request, 't').then(tag => {
@@ -19,3 +19,20 @@ exports.searchByTag = (request, callback) => {
         callback(err)
     })
 }
+exports.addUser = (request, callback) => {
+    let data
+    console.log(1)
+    db.checkCredentials(request).then(account => {
+        console.log(2)
+        this.account = account
+    }).then(account => {
+        console.log(3)
+        db.userExists(account)
+    }).then(account => {
+        console.log(4)
+        callback(null, account)
+    }).catch(err => {
+        callback(err)
+    })
+}
+exports.addFavourite
