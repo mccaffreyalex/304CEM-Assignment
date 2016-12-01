3d2c6fcbec54ab2c955acf9f80def27ea93f347c
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-12-01 13:15:28 +0000

    moved tests from flickr-info, updated to reflect new promise in /api

--- spec/flickr-spec.js
+++ spec/flickr-spec.js
@@ -3,37 +3,72 @@
 'use strict'
 const flickr = require('../modules/flickr')
 describe('The Flickr API', function () {
-        xit('should limit the number to 2', function (done) {
-            flickr.flickrSearch('London', function (err, searchResults) {
-                expect(err).toBe(null)
-                expect(searchResults.photos.perpage).toBe(2)
-                expect(searchResults.stat).toBe('ok')
-                done()
-            })
-        })
-        xit('should have status of ok', function (done) {
-            flickr.flickrSearch('London', function (err, searchResults) {
-                expect(err).toBe(null)
-                expect(searchResults.stat).toBe('ok')
-                done()
-            })
-        })
-        xit('should return 0 pages when no photos for the tag can be found', function (done) {
-            flickr.flickrSearch('randomtaghere', function (err, searchResults) {
-                expect(err).toBe(null)
-                expect(searchResults.photos.pages).toBe(0)
-                done()
-            })
-        })
-        xit('should contain the tag in the URL', function (done) {
-            flickr.flickrSearch('London', function (err, searchResults) {
-                expect(err).toBe(null)
-                expect(flickr.fullURL).toContain('tags=London')
-                done()
-            })
+    xit('should limit the number of photos to 1', function (done) {
+        flickr.searchByTag('London', function (err, searchResults) {
+            expect(err).toBe(null)
+            expect(searchResults.numberOfPhotos).toBe('1')
+            expect(searchResults.requestStatus).toBe('ok')
+            done()
         })
     })
-    ///tests
-    ///when searching for london, the url contains tags="london"
-    ///when searching, and no photos show up - undefinded
-    ///
+    xit('should have status of ok on successful requests', function (done) {
+        flickr.searchByTag('London', function (err, searchResults) {
+            expect(err).toBe(null)
+            expect(searchResults.requestStatus).toBe('ok')
+            done()
+        })
+    })
+    it('should return 0 pages when no photos for the tag can be found', function (done) {
+        flickr.searchByTag('randomtaghere', function (err, searchResults) {
+            expect(err).toBe
+
+            done()
+        })
+    })
+    xit('should contain the tag in the URL', function (done) {
+        flickr.searchByTag('London', function (err, searchResults) {
+            expect(err).toBe(null)
+            expect(flickr.fullURL).toContain('tags=London')
+            done()
+        })
+    })
+})
+describe('The Flickr-Info API', function () {
+    xit('confirm it has searched for the correct image', function (done) {
+        flickr.searchByID('24824103133', function (err, searchResults) {
+            expect(err).toBe(null)
+            expect(searchResults.photoID).toBe('24824103133')
+            done()
+        })
+    })
+    xit('should return error when no photo id is entered', function (done) {
+        flickr.searchByID('', function (err, searchResults) {
+            expect(err.stat).toBe('fail')
+            expect(err.code).toBe(1)
+            expect(err.message).toContain('not found')
+            done()
+        })
+    })
+    xit('should return error when an invalid photo_id is entered', function (done) {
+        flickr.searchByID('1', function (err, searchResults) {
+            expect(err.stat).toBe('fail')
+            expect(err.code).toBe(1)
+            expect(err.message).toContain('invalid ID')
+            done()
+        })
+    })
+    xit('should check the search was successful', function (done) {
+        flickr.searchByID('24824103133', function (err, searchResults) {
+            expect(err).toBe(null)
+            expect(searchResults.requestStatus).toBe('ok')
+            done()
+        })
+    })
+    xit('should contain the owners name', function (done) {
+        flickr.searchByID('24824103133', function (err, searchResults) {
+            expect(err).toBe(null)
+            expect(searchResults.ownerName).toBe('Alex McCaffrey')
+            done()
+        })
+    })
+})
