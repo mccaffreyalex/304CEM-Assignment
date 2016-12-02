df8bdd3a65ccb1c3f83d7977ccd4d4a06ca7c630
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-12-02 15:57:09 +0000

    updated

--- modules/flickr.js
+++ modules/flickr.js
@@ -4,15 +4,15 @@ exports.searchByTag = (tag, callback) => {
     const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=47b47ea9fe9d92ff9aac9cc70acb388a&tags=${tag}&min_upload_date=1477958400&per_page=1&format=json&nojsoncallback=1`
     request.get(url, (err, res, body) => {
         if (err) callback(Error('failed to make API call'))
-        console.log(url)
+            ///console.log(url)
         const json = JSON.parse(body)
-        console.log(json)
-        if (json.photos.total === 0) callback({
+        if (json.photos.total === '0') callback({
             message: 'No photos found with tag'
         })
         else {
             const data = {
-                searchTag: tag
+                apiURL: url
+                , searchTag: tag
                 , numberOfPhotos: `${json.photos.perpage}`
                 , photo_id: `${json.photos.photo[0].id}`
                 , owner: `${json.photos.photo[0].owner}`
@@ -28,7 +28,7 @@ exports.searchByID = (id, callback) => {
     const url = `https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=47b47ea9fe9d92ff9aac9cc70acb388a&photo_id=${id}&format=json&nojsoncallback=1`
     request.get(url, (err, res, body) => {
         if (err) callback(Error('failed to make API call'))
-        console.log(url)
+            //console.log(url)
         const json = JSON.parse(body)
         if (json.stat === 'fail') callback(json)
         else {
