cb4c0d9f50b4e7386ed76d337fc8df423acce262
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-12-02 15:57:30 +0000

    updated

--- spec/users-spec.js
+++ spec/users-spec.js
@@ -1,5 +1,4 @@
 'use strict'
-const user = require('../modules/auth')
 //describe('The add user function', function () {
 //        it('confirm the user has been added, but not confirmed', function (done) {
 //            user.validateUser('alex', , function (err, searchResults) {
@@ -9,9 +8,9 @@ const user = require('../modules/auth')
 //            })
 //        })
 //    })
-    ///check user is added
-    ///check user is not added
-    ///check user is confirmed
-    ///check user is not confirmed
-    ///check user deleted
-    ///check user not deleted
+///check user is added
+///check user is not added
+///check user is confirmed
+///check user is not confirmed
+///check user deleted
+///check user not deleted
