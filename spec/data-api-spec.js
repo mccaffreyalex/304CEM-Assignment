0c14dbab48c6b925e1d8ea48165866080b176da5
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-12-02 15:58:09 +0000

    included model file, started first test

--- spec/data-api-spec.js
+++ spec/data-api-spec.js
@@ -1,2 +1,20 @@
 'use strict'
-const api = require('../modules/data-api')
+const api = require('../modules/model')
+describe('My API', function () {
+        xit('should check the request was successful', function (done) {
+            api.searchByTag('London', function (err, searchResults) {
+                console.log(searchResults)
+                expect(err).toBe(null)
+            })
+        })
+    })
+    //it('should check that the request was successful', function (done) {
+    //    const date = '2016-12-01'
+    //    const location = 'London'
+    //    weather.searchWeather(location, date, function (err, searchResults) {
+    //        expect(err).toBe(null)
+    //        expect(searchResults.location).toBe(location)
+    //        expect(searchResults.date).toBe(date)
+    //        done()
+    //    })
+    //})
