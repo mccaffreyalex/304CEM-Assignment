71380760ba65b6503c297e78950bb2f6797252c8
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-07 02:47:55 +0000

    checks userExists/addUser/addToFavourites/deleteFavourite

--- modules/persistence.js
+++ modules/persistence.js
@@ -1,49 +1,54 @@
 'use strict'
 const schema = require('../schema/schema')
-exports.savePhoto = photoDetails => new Promise((resolve, reject) => {
-    if (!'id' in photoDetails && !'title' in photoDetails) reject(new Error('invalid photo object'))
-    const photo = new schema.Book(photoDetails)
-    movie.save((err, book) => {
-        if (err) reject(new Error('error when saving photo'))
-        resolve(book)
+const index = require('../index')
+exports.userExists = function (req, res, next) {
+    schema.userModel.find({
+        username: this.username
+    }, function (err, users) {
+        //    if (users.hasOwnProperty('username')) return (new Error('username already exists'))
+        if (!users.length) next()
+        else {
+            console.log('user exists: ', this.username)
+            next(new Error("User exists!"))
+        }
+        //    next()
     })
-})
-exports.addUser = details => new Promise((resolve, reject) => {
-    if (!'username' in details && !'password' in details) reject(new Error('invalid user object'))
-    const user = new schema.userModel(user)
-    user.username = details.username
-    user.password = details.password
-    user.save((err, user) => {
-        if (err) reject(new Error('error creating account'))
-        resolve()
+}
+exports.addUser = function (req, res, next) {
+    const user = new schema.userModel()
+    user.username = req.params.username
+    user.password = req.params.password
+    user.save(function (err) {
+        if (err) index.failure(res, next, 'User not saved', 500)
+        index.success(res, next, user)
     })
-})
-exports.userExists = account => new Promise((resolve, reject) => {
-    schema.User.find({
-        username: account.username
-    }, (err, docs) => {
-        if (docs.length) reject(new Error('This username is already taken'))
-        resolve()
-    })
-})
-exports.checkCredentials = credentials => new Promise((resolve, reject) => {
-    schema.User.find({
-        username: credentials.username
-    }, (err, docs) => {
-        if (err) reject(new Error('db error'))
-        if (docs.length) resolve(docs)
-        reject(new Error('invalid username'))
-    })
-})
+}
 exports.showUsers = function (err, callback) {
-    schema.User.find({}, function (err, users) {
+    schema.userModel.find({}, function (err, users) {
         if (err) return ('error could not find any users')
         return callback(null, users)
     })
 }
+exports.addToFavourites = function (req, res, next) {
+    const photo = new schema.photoModel()
+    photo.photoID = req.params.photoID
+    photo.save(function (err) {
+        if (err) index.failure(res, next, 'Photo not saved', 500)
+        index.success(res, next, photo)
+    })
+}
 exports.showFavourites = function (err, callback) {
-    schema.Photo.find({}, function (err, favourites) {
+    schema.photoModel.find({}, function (err, favourites) {
         if (err) return ('error could not find photos')
         return callback(null, favourites)
+    })
+}
+exports.deleteFavourite = function (photoID, callback) {
+    console.log('removing photo')
+    schema.photoModel.remove({
+        photoID: photoID
+    }, function (err, photo) {
+        if (err) throw err
+        return callback(null, photo)
     })
 }
