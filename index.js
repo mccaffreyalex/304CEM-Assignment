db526be78d2755216b767ed8632bfe32a7c0322a
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-07 04:56:39 +0000

    added .put feature

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const model = require('./modules/model')
 const schema = require('./schema/schema')
 const persistence = require('./modules/persistence')
 const port = process.env.PORT || 8080
-const status = {
+exports.status = {
     ok: 200
     , added: 201
     , badRequest: 400
@@ -27,19 +27,18 @@ exports.success = function success(res, next, data) {
 exports.failure = function failure(res, next, data, http_code) {
         respond(res, next, 'failure', data, http_code)
     }
-    ///////////////////////////////////////////////////////////////////////////////////////
+
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.acceptParser(server.acceptable))
 server.use(restify.authorizationParser())
-    ///////////////////////////////////////////////////////////////////////////////////////
+
 server.get('/api', model.search) // does search on city - yes ?q=london
 server.get('/users', model.showUsers) // shows users - yes
-server.post('/users', persistence.userExists, model.addUser) //add new user - yes
+server.post('/users', model.addUser) //add new user - yes
 server.get('/fav', model.showFavourites) //listing all favourites - yes
-server.get('/favauth', model.showFavourites) //listing all favourites with auth - no
 server.post('/fav', model.addToFavourites) //creating new favourite - yes, put {"id": in body}
 server.del('/fav/:photoID', model.deleteFavourite) //delete fav by id 8080:/fav/alex
     //server.put('/fav')
-    ///////////////////////////////////////////////////////////////////////////////////////
+server.put('/fav/.*', model.update)   ///////////////////////////////////////////////////////////////////////////////////////
 server.listen(port, err => console.log(err || `Server running at: http://localhost:${port}`))
