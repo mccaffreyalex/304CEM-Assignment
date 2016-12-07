3c33c698ee2e302048af9c31c8cc75f1afdf4bb9
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-07 04:57:16 +0000

    commented out

--- spec/flickr-spec.js
+++ spec/flickr-spec.js
@@ -1,73 +1,73 @@
-/*global require describe it xit expect */
-/*no eslint errors*/
-'use strict'
-const flickr = require('../modules/flickr')
-    describe('The Flickr API', function () {
-    it('should limit the number of photos to 1', function (done) {
-        flickr.searchByTag('London', function (err, searchResults) {
-            expect(err).toBe(null)
-            expect(searchResults.numberOfPhotos).toBe('1')
-            expect(searchResults.requestStatus).toBe('ok')
-            done()
-        })
-    })
-    it('should have status of ok on successful requests', function (done) {
-        flickr.searchByTag('London', function (err, searchResults) {
-            expect(err).toBe(null)
-            expect(searchResults.requestStatus).toBe('ok')
-            done()
-        })
-    })
-    it('should return 0 pages when no photos for the tag can be found', function (done) {
-        flickr.searchByTag('randomtaghere', function (err, searchResults) {
-            expect(err.message).toContain('tag')
-            done()
-        })
-    })
-    it('should contain the tag in the URL', function (done) {
-        flickr.searchByTag('London', function (err, searchResults) {
-            expect(err).toBe(null)
-            expect(searchResults.apiURL).toContain('tags=London')
-            done()
-        })
-    })
-})
-describe('The Flickr-Info API', function () {
-    it('confirm it has searched for the correct image', function (done) {
-        flickr.searchByID('24824103133', function (err, searchResults) {
-            expect(err).toBe(null)
-            expect(searchResults.photoID).toBe('24824103133')
-            done()
-        })
-    })
-    it('should return error when no photo id is entered', function (done) {
-        flickr.searchByID('', function (err, searchResults) {
-            expect(err.stat).toBe('fail')
-            expect(err.code).toBe(1)
-            expect(err.message).toContain('not found')
-            done()
-        })
-    })
-    it('should return error when an invalid photo_id is entered', function (done) {
-        flickr.searchByID('1', function (err, searchResults) {
-            expect(err.stat).toBe('fail')
-            expect(err.code).toBe(1)
-            expect(err.message).toContain('invalid ID')
-            done()
-        })
-    })
-    it('should check the search was successful', function (done) {
-        flickr.searchByID('24824103133', function (err, searchResults) {
-            expect(err).toBe(null)
-            expect(searchResults.requestStatus).toBe('ok')
-            done()
-        })
-    })
-    it('should contain the owners name', function (done) {
-        flickr.searchByID('24824103133', function (err, searchResults) {
-            expect(err).toBe(null)
-            expect(searchResults.ownerName).toBe('Alex McCaffrey')
-            done()
-        })
-    })
-})
+///*global require describe it xit expect */
+///*no eslint errors*/
+//'use strict'
+//const flickr = require('../modules/flickr')
+//    describe('The Flickr API', function () {
+//    it('should limit the number of photos to 1', function (done) {
+//        flickr.searchByTag('London', function (err, searchResults) {
+//            expect(err).toBe(null)
+//            expect(searchResults.numberOfPhotos).toBe('1')
+//            expect(searchResults.requestStatus).toBe('ok')
+//            done()
+//        })
+//    })
+//    it('should have status of ok on successful requests', function (done) {
+//        flickr.searchByTag('London', function (err, searchResults) {
+//            expect(err).toBe(null)
+//            expect(searchResults.requestStatus).toBe('ok')
+//            done()
+//        })
+//    })
+//    it('should return 0 pages when no photos for the tag can be found', function (done) {
+//        flickr.searchByTag('randomtaghere', function (err, searchResults) {
+//            expect(err.message).toContain('tag')
+//            done()
+//        })
+//    })
+//    it('should contain the tag in the URL', function (done) {
+//        flickr.searchByTag('London', function (err, searchResults) {
+//            expect(err).toBe(null)
+//            expect(searchResults.apiURL).toContain('tags=London')
+//            done()
+//        })
+//    })
+//})
+//describe('The Flickr-Info API', function () {
+//    it('confirm it has searched for the correct image', function (done) {
+//        flickr.searchByID('24824103133', function (err, searchResults) {
+//            expect(err).toBe(null)
+//            expect(searchResults.photoID).toBe('24824103133')
+//            done()
+//        })
+//    })
+//    it('should return error when no photo id is entered', function (done) {
+//        flickr.searchByID('', function (err, searchResults) {
+//            expect(err.stat).toBe('fail')
+//            expect(err.code).toBe(1)
+//            expect(err.message).toContain('not found')
+//            done()
+//        })
+//    })
+//    it('should return error when an invalid photo_id is entered', function (done) {
+//        flickr.searchByID('1', function (err, searchResults) {
+//            expect(err.stat).toBe('fail')
+//            expect(err.code).toBe(1)
+//            expect(err.message).toContain('invalid ID')
+//            done()
+//        })
+//    })
+//    it('should check the search was successful', function (done) {
+//        flickr.searchByID('24824103133', function (err, searchResults) {
+//            expect(err).toBe(null)
+//            expect(searchResults.requestStatus).toBe('ok')
+//            done()
+//        })
+//    })
+//    it('should contain the owners name', function (done) {
+//        flickr.searchByID('24824103133', function (err, searchResults) {
+//            expect(err).toBe(null)
+//            expect(searchResults.ownerName).toBe('Alex McCaffrey')
+//            done()
+//        })
+//    })
+//})
--- spec/users-spec.js
+++ spec/users-spec.js
@@ -1,16 +1,43 @@
-'use strict'
-//describe('The add user function', function () {
-//        it('confirm the user has been added, but not confirmed', function (done) {
-//            user.validateUser('alex', , function (err, searchResults) {
-//                expect(err).toBe(null)
-//                expect(searchResults.photo.id).toBe('24824103133')
-//                done()
+//'use strict'
+//const persistence = require('../modules/persistence')
+//const schema = require('../schema//schema')
+//describe('Users', () => {
+//    beforeEach(done => {
+//        schema.userModel.remove({}, err => {
+//            if (err) expect(true).toBe(false)
+//            new schema.userModel({
+//                username: 'alex'
+//                , password: 'p5ssword'
+//            }).save((err, userModel) => {
+//                if (err) expect(true).toBe(false)
+//                schema.userModel.count({}, (err, count) => {
+//                    if (err) expect(true).toBe(false)
+//                    expect(count).toBe(1)
+//                    done()
+//                })
 //            })
 //        })
 //    })
-///check user is added
-///check user is not added
-///check user is confirmed
-///check user is not confirmed
-///check user deleted
-///check user not deleted
+//})
+//describe('add', () => {
+//        it('should add a user', done => {
+//            const user = {
+//                username: 'alex'
+//                , password: 'p5ssword'
+//            }
+//            persistence.addUser.add(user, (err, data) => {
+//                if (err) expect(true).toBe(false)
+//                schema.userModel.count({}, (err, count) => {
+//                    if (err) expect(true).toBe(false)
+//                    expect(count).toBe(2)
+//                    done()
+//                })
+//            })
+//        })
+//    })
+//    ///check user is added
+//    ///check user is not added
+//    ///check user is confirmed
+//    ///check user is not confirmed
+//    ///check user deleted
+//    ///check user not deleted
