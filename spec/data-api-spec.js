971ca440e95d6f56129c6919562786b0883315d0
alexmccaffrey-mac <mccaffra@uni.coventy.ac.uk>
2016-12-08 20:28:35 +0000

    frisbee AT on search, need to test where location is not found

--- spec/data-api-spec.js
+++ spec/data-api-spec.js
@@ -1,23 +1,34 @@
-//'use strict'
-//const model = require('../modules/model')
-//
-//describe('My API', function() {
-//    it('should check the request was successful', function(done) {
-//        model.searchByTag('api?t=coventry', function(err, searchResults) {
-//            console.log(searchResults)
-//            console.log(err)
-//            expect(err).toBe(null)
-//            done()
-//        })
-//    })
-//})
-//    it('should check that the request was successful', function (done) {
-//        const date = '2016-12-01'
-//        const location = 'London'
-//        weather.searchWeather(location, date, function (err, searchResults) {
-//            expect(err).toBe(null)
-//            expect(searchResults.location).toBe(location)
-//            expect(searchResults.date).toBe(date)
-//            done()
-//        })
-//    })
+const frisby = require('frisby')
+const hostURL = 'http://localhost:8080/'
+const correctTag = 'coventry'
+const incorrectTag = 'randomtaghere'
+frisby.create('API /GET should return 200 for a successful search')
+    .get(hostURL + 'api?t=' + correctTag)
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON(
+             {
+              "results": {
+                "apiURL": "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=47b47ea9fe9d92ff9aac9cc70acb388a&tags=coventry&min_upload_date=1477958400&per_page=1&format=json&nojsoncallback=1",
+                "searchTag": "coventry",
+                "numberOfPhotos": "1",
+                "photo_id": "30662421194",
+                "owner": "79839874@N04",
+                "title": "Day 342 of 366 - Old Canley Hall!",
+                "url": "http://farm1.staticflickr.com/58/30662421194_75f1465f1e.jpg",
+                "requestStatus": "ok"
+              }
+            }   
+        )
+.toss()
+
+frisby.create('My API should return 400 for a failed search')
+    .get(hostURL + 'api?t=' + incorrectTag)
+    .expectStatus(400)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON(
+            {
+                "message": "No photos found with tag"
+            }
+)
+.toss()
