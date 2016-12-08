41ac154846e5b06abb0ca1602bc208b91dfc56d0
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-08 01:26:43 +0000

    removed ObjectID

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,27 @@
 'use strict'
 const mongoose = require('mongoose')
+
 const credentials = {
-    user: 'alex'
+	user: 'alex'
     , pass: 'alex'
 }
+
 mongoose.connect(`mongodb://${credentials.user}:${credentials.pass}@ds113958.mlab.com:13958/304db`)
+
 mongoose.Promise = global.Promise
+
 const Schema = mongoose.Schema
-    , ObjectId = Schema.ObjectId
+
 const UserSchema = new Schema({
-    username: String
+	username: String
     , password: String
 })
+
 exports.userModel = mongoose.model('users', UserSchema)
+
 const photoSchema = new Schema({
-    photoID: String,
-    location: String
+	photoID: String,
+	location: String
 })
+
 exports.photoModel = mongoose.model('Photo', photoSchema)
