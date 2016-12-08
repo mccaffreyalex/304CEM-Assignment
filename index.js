c9bbe3c3927873f6393c602e7a1a5e4cf70d8f17
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-08 01:23:59 +0000

    added model.deleteUser

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 'use strict'
+//---------------------------------------------------------------------
 const restify = require('restify')
+const index = require('./index')
 const server = restify.createServer()
 const model = require('./modules/model')
-const schema = require('./schema/schema')
-const persistence = require('./modules/persistence')
-const port = process.env.PORT || 8080
+const defaultPort = 8080
+const port = process.env.PORT || defaultPort
+//---------------------------------------------------------------------
+
 exports.status = {
-    ok: 200
+	ok: 200
     , added: 201
     , badRequest: 400
 }
 
 function respond(res, next, status, data, http_code) {
-    const response = {
-        'status': status
+	const response = {
+		'status': status
         , 'data': data
-    }
-    res.setHeader('content-type', 'application/json')
-    res.writeHead(http_code)
-    res.end(JSON.stringify(response))
-    return next()
+	}
+
+	res.setHeader('content-type', 'application/json')
+	res.writeHead(http_code)
+	res.end(JSON.stringify(response))
+	return next()
 }
 exports.success = function success(res, next, data) {
-    respond(res, next, 'success', data, 200)
+	respond(res, next, 'success', data, index.status.ok)
 }
 exports.failure = function failure(res, next, data, http_code) {
-        respond(res, next, 'failure', data, http_code)
-    }
-
+	respond(res, next, 'failure', data, http_code)
+}
+//---------------------------------------------------------------------
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.acceptParser(server.acceptable))
 server.use(restify.authorizationParser())
-
+//---------------------------------------------------------------------
 server.get('/api', model.search) // does search on city - yes ?q=london
 server.get('/users', model.showUsers) // shows users - yes
 server.post('/users', model.addUser) //add new user - yes
+server.del('/users', model.deleteUser) // delete user
 server.get('/fav', model.showFavourites) //listing all favourites - yes
 server.post('/fav', model.addToFavourites) //creating new favourite - yes, put {"id": in body}
 server.del('/fav/:photoID', model.deleteFavourite) //delete fav by id 8080:/fav/alex
-    //server.put('/fav')
-server.put('/fav/.*', model.update)   ///////////////////////////////////////////////////////////////////////////////////////
+//server.put('/fav') - need to add this later
+server.put('/fav/.*', model.update) //update photoID with location
+    //---------------------------------------------------------------------
 server.listen(port, err => console.log(err || `Server running at: http://localhost:${port}`))
--- modules/persistence.js
+++ modules/persistence.js
@@ -1,66 +1,80 @@
 'use strict'
 const schema = require('../schema/schema')
 const index = require('../index')
-exports.userExists = function (req, res, next) {
-    schema.userModel.find({
-        username: this.username
-    }, function (err, users) {
-        //    if (users.hasOwnProperty('username')) return (new Error('username already exists'))
-        if (!users.length) next()
-        else {
-            console.log('user exists: ', this.username)
-            next(new Error("User exists!"))
-        }
-        //    next()
-    })
+const status = {
+	internalServerError: 500
 }
-exports.addUser = function (req, res, next) {
-    const user = new schema.userModel()
-    user.username = req.params.username
-    user.password = req.params.password
-    user.save(function (err) {
-        if (err) index.failure(res, next, 'User not saved', 500)
-        index.success(res, next, user)
-    })
+
+
+exports.addUser = function(req, res, next) {
+	const user = new schema.userModel()
+
+	user.username = req.params.username
+	user.password = req.params.password
+	user.save(function(err) {
+		if (err) index.failure(res, next, 'User not saved', status.internalServerError)
+		index.success(res, next, user)
+	})
 }
-exports.showUsers = function (err, callback) {
-    schema.userModel.find({}, function (err, users) {
-        if (err) return ('error could not find any users')
-        return callback(null, users)
-    })
+
+
+exports.showUsers = function(users, callback) {
+	schema.userModel.find({}, function(err, users) {
+		if (err) throw err
+		return callback(null, users)
+	})
 }
-exports.addToFavourites = function (req, res, next) {
-    const photo = new schema.photoModel()
-    photo.photoID = req.params.photoID
-    photo.location = req.params.location
-    photo.save(function (err) {
-        if (err) index.failure(res, next, 'Photo not saved', 500)
-        index.success(res, next, photo)
-    })
+
+exports.deleteUser = function(username, callback) {
+	console.log('removing photo')
+	schema.userModel.remove({
+		username: username
+	}, function(err, user) {
+		if (err) throw err
+		return callback(null, user)
+	})
 }
-exports.showFavourites = function (err, callback) {
-    schema.photoModel.find({}, function (err, favourites) {
-        if (err) return ('error could not find photos')
-        return callback(null, favourites)
-    })
+
+
+exports.addToFavourites = function(req, res, next) {
+	const photo = new schema.photoModel()
+
+	photo.photoID = req.params.photoID
+	photo.location = req.params.location
+	photo.save(function(err) {
+		if (err) index.failure(res, next, 'Photo not saved', status.internalServerError)
+		index.success(res, next, photo)
+	})
 }
-exports.deleteFavourite = function (photoID, callback) {
-    console.log('removing photo')
-    schema.photoModel.remove({
-        photoID: photoID
-    }, function (err, photo) {
-        if (err) throw err
-        return callback(null, photo)
-    })
+
+
+exports.showFavourites = function(favourites, callback) {
+	schema.photoModel.find({}, function(err, favourites) {
+		if (err) return 'error could not find photos'
+		return callback(null, favourites)
+	})
 }
-exports.update = function (photoID, location, callback) {
-    console.log('Changing location of photo:' + photoID)
-    schema.photoModel.findOneAndUpdate({
-        photoID: photoID
-    }, {
-        'location': location
-    }, function (err, photos) {
-        if (err) throw err
-        return callback(null, photos)
-    })
+
+
+exports.deleteFavourite = function(photoID, callback) {
+	console.log('removing photo')
+	schema.photoModel.remove({
+		photoID: photoID
+	}, function(err, photo) {
+		if (err) throw err
+		return callback(null, photo)
+	})
+}
+
+
+exports.update = function(photoID, location, callback) {
+	console.log('Changing location of photo:' + photoID)
+	schema.photoModel.findOneAndUpdate({
+		photoID: photoID
+	}, {
+		'location': location
+	}, function(err, photos) {
+		if (err) throw err
+		return callback(null, photos)
+	})
 }
