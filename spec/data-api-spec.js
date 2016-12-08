d6915b0b2eff31e986b681dcf6e5d7e4bca0c125
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-08 23:46:16 +0000

    changed correctTag to Birmingham, reduced expectJSON(results_

--- spec/data-api-spec.js
+++ spec/data-api-spec.js
@@ -1,6 +1,6 @@
 const frisby = require('frisby')
 const hostURL = 'http://localhost:8080/'
-const correctTag = 'coventry'
+const correctTag = 'Birmingham'
 const incorrectTag = 'randomtaghere'
 frisby.create('API /GET should return 200 for a successful search')
     .get(hostURL + 'api?t=' + correctTag)
@@ -9,18 +9,14 @@ frisby.create('API /GET should return 200 for a successful search')
     .expectJSON(
              {
               "results": {
-                "apiURL": "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=47b47ea9fe9d92ff9aac9cc70acb388a&tags=coventry&min_upload_date=1477958400&per_page=1&format=json&nojsoncallback=1",
-                "searchTag": "coventry",
-                "numberOfPhotos": "1",
-                "photo_id": "30662421194",
-                "owner": "79839874@N04",
-                "title": "Day 342 of 366 - Old Canley Hall!",
-                "url": "http://farm1.staticflickr.com/58/30662421194_75f1465f1e.jpg",
+                "searchTag": correctTag,
                 "requestStatus": "ok"
               }
-            }   
+            }
         )
 .toss()
+
+//location failed
 
 frisby.create('My API should return 400 for a failed search')
     .get(hostURL + 'api?t=' + incorrectTag)
