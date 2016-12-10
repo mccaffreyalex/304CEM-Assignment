fe2106646e6d45ed36651eff202f775c478ac715
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-10 05:00:20 +0000

    created file, works between the index.js and model.js.

---
+++ modules/handler.js
@@ -0,0 +1,122 @@
+'use strict'
+const model = require('./model')
+const status = {ok: 200, added: 201, badRequest: 400}
+
+exports.search = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'GET')
+	model.searchByTag(req, (err, data) => {
+		err ? res.send(status.badRequest, err) : res.send(status.ok, data)
+		res.end()
+	})
+}
+
+/**
+     * Shows a list of users stored in the 'users' collection
+     * @function
+     * @param {string} req - HTTP request
+     * @param {string} res - HTTP response
+     * @returns {[[Type]]} [[Description]]
+     */
+exports.getUser = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'GET')
+        //req.authorization.basic.username
+	model.getUser(req.authorization.basic.username, (err, user) => {
+		err ? res.send(status.badRequest, err) : res.send(status.ok, user)
+		res.end()
+	})
+}
+
+/**
+     * Adds new user to 'users' collection
+     * @function
+     * @param {string} req - HTTP request
+     * @param {string} res - HTTP response
+     */
+exports.addUser = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'POST')
+	model.addUser(req.body.username, req.body.password, (err, user) => {
+		err ? res.send(status.badRequest, err) : res.send(status.added, user)
+		res.end()
+	})
+}
+
+exports.updateUser = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'PUT')
+	model.updateUser(req.authorization.basic.username,req.body.password, (err, user) => {
+		err ? res.send(status.badRequest, err) : res.send(user)
+		res.end()
+	})
+}
+
+exports.deleteUser = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'DELETE')
+	model.deleteUser(req.authorization.basic.username, (err, message) => {
+		err ? res.send(status.badRequest, err) : res.send(status.ok, message)
+		res.end()
+	})
+}
+
+/**
+     * Shows all records in the photos collection
+     * @function
+     * @param {string} req - HTTP request
+     * @param {string} res - HTTP response
+     * @returns {[[Type]]} [[Description]]
+     */
+exports.getFavPhotos = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'GET')
+	model.getFavPhotos(req.authorization.basic.username, (err, photos) => {
+		err ? res.send(status.badRequest, err) : res.send(status.ok, photos)
+		res.end()
+	})
+}
+
+/**
+     * Adds a photo to photos collection
+     * @function
+     * @param {string} req - HTTP request
+     * @param {string} res - HTTP response
+     * @returns {[[Type]]} [[Description]]
+     */
+exports.addFavPhoto = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'POST')
+	model.addFavPhoto(req.authorization.basic.username, req.body.photoID, req.body.location, (err, data) => {
+		err ? res.send(status.badRequest, err) : res.send(status.added, data)
+		res.end()
+	})
+}
+
+/**
+     * Deletes a photo from the 'photos' collection
+     * @function
+     * @param {string} req - HTTP request
+     * @param {string} res - HTTP response
+     * @param {number} photoID - id of photo
+     * @returns {[[Type]]} [[Description]]
+     */
+exports.deleteFavPhotos = (req, res) => {
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('accepts', 'DELETE')
+	model.deleteFavPhotos(req.authorization.basic.username, req.body.photoID, (err, data) => {
+		err ? res.send(err) : res.send(data)
+		res.end()
+	})
+}
+
+/**
+     * Updates photo id location based off the photoID
+     * @function
+     * @param {string} req - HTTP request
+     * @param {string} res - HTTP response
+     * @param {photoID} req.params.photoID - photoID
+     * @param {location} req.params.location - location
+     * @returns {[[Type]]} [[Description]]
+     */
+
--- modules/model.js
+++ modules/model.js
@@ -1,13 +1,8 @@
 'use strict'
 //---------------------------------------------------------------------
 const api = require('./data-api')
-const model = require('./model')
+
 const persistence = require('./persistence')
-const status = {
-	ok: 200
-        , added: 201
-        , badRequest: 400
-}
 //---------------------------------------------------------------------
     /**
      * Callback for searching Flickr & Apixu weather in a promise chain
@@ -29,114 +24,59 @@ exports.searchByTag = (request, callback) => {
 	})
 }
 
-/**
-     * Parses tag into searchByTag
-     * @function
-     * @param {string} req - HTTP request
-     * @param {string} res - HTTP response
-     * @returns {data} photo results, photo data, weather data combined
-     */
-exports.search = (req, res) => {
-	res.setHeader('content-type', 'application/json')
-	res.setHeader('accepts', 'GET')
-	model.searchByTag(req, (err, data) => {
-		err ? res.send(status.badRequest, err) : res.send(status.ok, data)
-		res.end()
+exports.addUser = (username, password, callback) => {
+	persistence.addUser(username, password).then(user => {
+		callback(null, user)
+	}).catch(err => {
+		callback(err)
+	})
+}
+exports.getUser = (username, callback) => {
+	persistence.getUser(username).then(user => {
+		callback(null, user)
+	}).catch(err => {
+		callback(err)
+	})
+}
+
+exports.deleteUser = (username, callback) => {
+	persistence.deleteUser(username).then(user => {
+		callback(null, user)
+	}).catch(err => {
+		callback(err)
+	})
+}
+
+exports.updateUser = (username, password, callback) => {
+	persistence.updateUser(username, password).then(user => {
+		callback(null, user)
+	}).catch(err => {
+		callback(err)
+	})
+}
+
+exports.addFavPhoto = (username, photoID, location, callback) => {
+	persistence.addFavPhoto(username, photoID, location).then(photo => {
+		callback(null, photo)
+	}).catch(err => {
+		callback(err)
+	})
+}
+
+exports.getFavPhotos = (username, callback) => {
+	persistence.getFavPhotos(username).then(photo => {
+		callback(null, photo)
+	}).catch(err => {
+		callback(err)
+	})
+}
+
+exports.deleteFavPhotos = (username, photoID, callback) => {
+	persistence.deleteFavPhotos(username, photoID).then(photo => {
+		callback(null, photo)
+	}).catch(err => {
+		callback(err)
 	})
 }
 
 
-/**
-     * Shows a list of users stored in the 'users' collection
-     * @function
-     * @param {string} req - HTTP request
-     * @param {string} res - HTTP response
-     * @returns {[[Type]]} [[Description]]
-     */
-exports.showUsers = (req, res) => {
-	res.setHeader('content-type', 'application/json')
-	res.setHeader('accepts', 'GET')
-	persistence.showUsers(req, (err, data) => {
-		err ? res.send(err) : res.send(data)
-		res.end()
-	})
-}
-
-
-/**
-     * Adds new user to 'users' collection
-     * @function
-     * @param {string} req - HTTP request
-     * @param {string} res - HTTP response
-     */
-exports.addUser = persistence.addUser
-
-exports.deleteUser = (req, res) => {
-	res.setHeader('content-type', 'application/json')
-	res.setHeader('accepts', 'DELETE')
-	persistence.deleteUser(req.params.username, (err, data) => {
-		err ? res.send(err) : res.send(data)
-		res.end()
-	})
-}
-
-
-/**
-     * Shows all records in the photos collection
-     * @function
-     * @param {string} req - HTTP request
-     * @param {string} res - HTTP response
-     * @returns {[[Type]]} [[Description]]
-     */
-exports.showFavourites = (req, res) => {
-	res.setHeader('content-type', 'application/json')
-	res.setHeader('accepts', 'GET')
-	persistence.showFavourites(req, (err, data) => {
-		err ? res.send(err) : res.send(data)
-		res.end()
-	})
-}
-
-/**
-     * Adds a photo to photos collection
-     * @function
-     * @param {number} photoID - id of photo
-     * @returns {[[Type]]} [[Description]]
-     */
-exports.addToFavourites = persistence.addToFavourites
-
-/**
-     * Deletes a photo from the 'photos' collection
-     * @function
-     * @param {string} req - HTTP request
-     * @param {string} res - HTTP response
-     * @param {number} photoID - id of photo
-     * @returns {[[Type]]} [[Description]]
-     */
-exports.deleteFavourite = (req, res) => {
-	res.setHeader('content-type', 'application/json')
-	res.setHeader('accepts', 'DELETE')
-	persistence.deleteFavourite(req.params.photoID, (err, data) => {
-		err ? res.send(err) : res.send(data)
-		res.end()
-	})
-}
-
-
-/**
-     * Updates photo id location based off the photoID
-     * @function
-     * @param {string} req - HTTP request
-     * @param {string} res - HTTP response
-     * @param {photoID} req.params.photoID - photoID
-     * @param {location} req.params.location - location
-     * @returns {[[Type]]} [[Description]]
-     */
-exports.update = (req, res) => {
-	res.setHeader('content-type', 'application/json')
-	res.setHeader('accepts', 'PUT')
-	persistence.update(req.params.photoID, req.params.location, (err, data) => {
-
-		err ? res.send(status.badRequest, err) : res.send(data)
-	})
-}
