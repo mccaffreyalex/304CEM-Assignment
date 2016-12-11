ad1281ad7f658cc4da67ab728b1dfc3b3ca28855
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-11 01:59:47 +0000

    added generateHash and validPassword functions

--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,8 @@
 'use strict'
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 const credentials = {
-	user: 'alex'
+    user: 'alex'
     , pass: 'alex'
 }
 
@@ -12,24 +12,33 @@ mongoose.Promise = global.Promise
 
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema({
-	username: String
+const userSchema = new Schema({
+    username: String
     , password: String
 })
 
-exports.userModel = mongoose.model('users', UserSchema)
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(9))
+}
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
+exports.userModel = mongoose.model('users', userSchema)
 
 const photoSchema = new Schema({
-	username: String,
-	photoID: String,
-	location: String
+    username: String,
+    photoID: String,
+    location: String
 })
 
 exports.photoModel = mongoose.model('Photo', photoSchema)
+
     // using this to close the mongoose connection when running code coverage.
 process.on('SIGINT', function() {
-	mongoose.disconnect(function() {
-		console.log('Mongoose default connection disconnected through app termination')
-		process.exit(0)
-	})
+    mongoose.disconnect(function() {
+        console.log('Mongoose default connection disconnected through app termination')
+        process.exit(0)
+    })
 })
