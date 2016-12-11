6a2cc018d465dd6016f2f30126270e2aff364971
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-11 07:10:08 +0000

    updated to include extra error codes

--- modules/weather.js
+++ modules/weather.js
@@ -2,24 +2,19 @@
 const request = require('request')
 
 exports.searchWeather = (location, date, callback) => {
-	const url = `http://api.apixu.com/v1/history.json?key=ceac95eb930640de828164828162610&q=${location}&dt=${date}`
+    const url = `http://api.apixu.com/v1/history.json?key=ceac95eb930640de828164828162610&q=${location}&dt=${date}`
 
-	request.get(url, (err, res, body) => {
-		const json = JSON.parse(body)
-
-		if (err) console.log(err)
-		else if (json.hasOwnProperty('error')) callback({
-			code: 1003
-            , message: 'location missing'
+    request.get(url, (err, res, body) => {
+        const json = JSON.parse(body)
+        if (err) console.log(err)
+        else if (json.hasOwnProperty('error')) callback({
+            code: 1003
+            , message: 'Location missing/no matching city/this key is only valid for 30 days only'
             , stat: 'fail'
-		})
-		else if (json.hasOwnProperty('error')) callback({
-			code: 1007
-            , message: 'dt missing or in wrong format (yyyy-MM-dd)'
-		})
-		else {
-			const data = {
-				location: `${json.location.name}`
+        })
+        else {
+            const data = {
+                location: `${json.location.name}`
                 , country: `${json.location.country}`
                 , date: `${json.forecast.forecastday[0].date}`
                 , weatherCondition: `${json.forecast.forecastday[0].day.condition.text}`
@@ -28,7 +23,8 @@ exports.searchWeather = (location, date, callback) => {
                 , sunSet: `${json.forecast.forecastday[0].astro.sunset}`
             , }
 
-			callback(null, data)
-		}
-	})
+            callback(null, data)
+        }
+    })
 }
+
