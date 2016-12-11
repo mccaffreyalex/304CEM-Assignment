0c6d9ea29d1cd977a9bc43f59837ecda601d08b4
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-11 07:11:15 +0000

    reformatted

--- index.js
+++ index.js
@@ -18,22 +18,23 @@ const port = process.env.PORT || defaultPort
      * @param {string} http_code - HTTP code
      * @returns
      */
-function respond(res, next, status, data, http_code) {
-    const response = {
-        'status': status
-        , 'data': data
-    }
 
-    res.setHeader('content-type', 'application/json')
-    res.writeHead(http_code)
-    res.end(JSON.stringify(response))
-    return next()
+function respond(res, next, status, data, http_code) {
+	const response = {
+		'status': status
+        , 'data': data
+	}
+
+	res.setHeader('content-type', 'application/json')
+	res.writeHead(http_code)
+	res.end(JSON.stringify(response))
+	return next()
 }
 exports.success = function success(res, next, data) {
-    respond(res, next, 'success', data, index.status.ok)
+	respond(res, next, 'success', data, index.status.ok)
 }
 exports.failure = function failure(res, next, data, http_code) {
-    respond(res, next, 'failure', data, http_code)
+	respond(res, next, 'failure', data, http_code)
 }
 //---------------------------------------------------------------------
 server.use(restify.queryParser())
@@ -41,17 +42,15 @@ server.use(restify.bodyParser())
 server.use(restify.acceptParser(server.acceptable))
 server.use(restify.authorizationParser())
 //---------------------------------------------------------------------
-server.get('/api', handler.search) // does search on city - yes ?q=london
-server.get('/user', handler.getUser) // shows users DONE
-server.post('/user', auth.authorize, handler.addUser) //add new user - DONE
-server.del('/user', handler.deleteUser) // delete user DONE
-server.put('/user', handler.updateUser) //update user DONE
-server.get('/fav', handler.getFavPhotos) //listing all favourites - DONE
-server.post('/fav', handler.addFavPhoto) //creating new favourite -  DONE
-server.del('/fav', handler.deleteFavPhotos) //delete fav by id 8080:/fav/alex
-//server.put('/fav') - need to add this later
-
-    //---------------------------------------------------------------------
+server.get('/api', handler.search)
+server.get('/user', handler.getUser)
+server.post('/user', handler.addUser)
+server.del('/user', handler.deleteUser)
+server.put('/user', handler.updateUser)
+server.get('/fav', handler.getFavPhotos)
+server.post('/fav', handler.addFavPhoto)
+server.del('/fav', handler.deleteFavPhotos)
+//---------------------------------------------------------------------
 
 /**
      * Parses tag into searchByTag
