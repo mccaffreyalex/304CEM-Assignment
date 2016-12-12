64d0d4efc26ad268c432f51bfa70890b28c697bb
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-12 22:24:55 +0000

    re ran jsdoc *

--- modules/weather.js
+++ modules/weather.js
@@ -2,6 +2,10 @@
 const request = require('request')
 
 /**
+*@module Weather Module
+*/
+
+/**
 *Searching Apixu for weather conditions based on location & date
 *@param {string} location - location
 *@param {string} date - date
@@ -11,24 +15,24 @@ const request = require('request')
 */
 
 exports.searchWeather = (location, date, callback) => {
-	const url = `http://api.apixu.com/v1/history.json?key=ceac95eb930640de828164828162610&q=${location}&dt=${date}`
+    const url = `http://api.apixu.com/v1/history.json?key=ceac95eb930640de828164828162610&q=${location}&dt=${date}`
 
-	request.get(url, (err, res, body) => {
-		const json = JSON.parse(body)
+    request.get(url, (err, res, body) => {
+        const json = JSON.parse(body)
         /* istanbul ignore next */ //only fails if Apixu API is down
 
-		if (err) console.log(err)
-		else if (json.hasOwnProperty('error')) callback({
-			code: 1003
+        if (err) console.log(err)
+        else if (json.hasOwnProperty('error')) callback({
+            code: 1003
             , message: 'Location missing/no matching city/this key is only valid for 30 days only'
             , stat: 'fail'
-		})
-		else if (json.hasOwnProperty('error')) callback({
-			code: 1007, message: 'dt missing or in wrong format (yyyy-MM-dd)'
-		})
-		else {
-			const data = {
-				location: `${json.location.name}`
+        })
+        else if (json.hasOwnProperty('error')) callback({
+            code: 1007, message: 'dt missing or in wrong format (yyyy-MM-dd)'
+        })
+        else {
+            const data = {
+                location: `${json.location.name}`
                 , country: `${json.location.country}`
                 , date: `${json.forecast.forecastday[0].date}`
                 , weatherCondition: `${json.forecast.forecastday[0].day.condition.text}`
@@ -37,8 +41,8 @@ exports.searchWeather = (location, date, callback) => {
                 , sunSet: `${json.forecast.forecastday[0].astro.sunset}`
             , }
 
-			callback(null, data)
-		}
-	})
+            callback(null, data)
+        }
+    })
 }
 
